15dc39ec025bd39e7de0088b3df0b8beb98a8ca6
zwazh <sabrinaeyre@gmail.com>
2015-04-13 10:28:32 -0500

    Update api.py

--- api.py
+++ api.py
@@ -5,22 +5,9 @@ import urllib2, json
 # Replace this with your own
 API_KEY = '6762c5fba6cf496e91d527f06213e05f'
 
-def sample_function(name):
-    print 'I am a sample function'
-    print 'My name is %s' % name
-    return
-
 ########## YOUR CODE GOES HERE ##########
 
-def get_bills(state, type):
-    response = urllib2.urlopen('http://openstates.org/api/v1/bills/?state=mo&apikey=6762c5fba6cf496e91d527f06213e05f')
-    json_object = json.load(response)
-
-    for bills in json_object:
-      if bills['state'] == 'mo': 
-        if 'Transportation' in bills['subjects']:
-          print bills['title']
-
+def get_bills(state, subjects):
     '''
     This function should accomplish the following tasks:
       - Open a connection to the /bills/ endpoing of Sunlight Foundation's OpenStates API:
@@ -28,11 +15,17 @@ def get_bills(state, type):
       - Retrieve an API response with bills from the current legislative session in Missouri
       - print the titles of bills that contain "Transportation" in the "subjects" list
     '''
+    response = urllib2.urlopen('http://openstates.org/api/v1/bills/?state=%s&&search_window=session&subject=%s&apikey=%s' % (state, subjects, API_KEY))
+    json_object = json.load(response)
+
+    print json_object
+
+    for bills in json_object:
+      print bills['title'] 
+
     return
 
 ########## RUN FUNCTIONS HERE ##########
-
-sample_function('Chase')
 
 # Uncomment this to run your function with the appropriate arguments, as specified above
 get_bills('mo', 'Transportation')
